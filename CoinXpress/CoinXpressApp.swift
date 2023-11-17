9211e1831ab86fd7dd12c5f7edc5f83298948b8a
misaki1301 <paul.frankpc@gmail.com>
2023-11-17 14:27:32 -0500

    Initial Commit

---
+++ CoinXpress/CoinXpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinXpressApp.swift
+//  CoinXpress
+//
+//  Created by Paul Frank Pacheco Carpio on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinXpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
